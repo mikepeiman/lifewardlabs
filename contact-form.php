10a744da7b4328547ddbc9bd8985e2f3d3b021b7
Mike Peiman <mikepeiman@gmail.com>
2015-09-11 14:45:07 -0400

    updated modal content and backgrounds. Added photo alongside panel in 'about' modal. could not validate my php contact form script locally, intend to simply deploy and troubleshoot if it doesn't work live. realistically, I'm not expecting more than a few visits initially anyways.

--- contact-form.php
+++ contact-form.php
@@ -6,9 +6,9 @@ if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
  
-    $email_to = "you@yourdomain.com";
+    $email_to = "mike@lifewardlabs.com";
  
-    $email_subject = "Your email subject line";
+    $email_subject = "Message from contact form on LifewardLabs.com";
  
      
  
@@ -34,13 +34,9 @@ if(isset($_POST['email'])) {
  
     // validation expected data exists
  
-    if(!isset($_POST['first_name']) ||
- 
-        !isset($_POST['last_name']) ||
+    if(!isset($_POST['name']) ||
  
         !isset($_POST['email']) ||
- 
-        !isset($_POST['telephone']) ||
  
         !isset($_POST['comments'])) {
  
@@ -50,13 +46,9 @@ if(isset($_POST['email'])) {
  
      
  
-    $first_name = $_POST['first_name']; // required
- 
-    $last_name = $_POST['last_name']; // required
+    $name = $_POST['name']; // required
  
     $email_from = $_POST['email']; // required
- 
-    $telephone = $_POST['telephone']; // not required
  
     $comments = $_POST['comments']; // required
  
@@ -74,21 +66,16 @@ if(isset($_POST['email'])) {
  
     $string_exp = "/^[A-Za-z .'-]+$/";
  
-  if(!preg_match($string_exp,$first_name)) {
+  if(!preg_match($string_exp,$name)) {
  
-    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
- 
-  }
- 
-  if(!preg_match($string_exp,$last_name)) {
- 
-    $error_message .= 'The Last Name you entered does not appear to be valid.<br />';
+    $error_message .= 'The name you entered does not appear to be valid.<br />';
  
   }
+
  
-  if(strlen($comments) < 2) {
+  if(strlen($comments) < 3) {
  
-    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+    $error_message .= 'Pretty short message... Can you say more about that?<br />';
  
   }
  
@@ -112,13 +99,9 @@ if(isset($_POST['email'])) {
  
      
  
-    $email_message .= "First Name: ".clean_string($first_name)."\n";
- 
-    $email_message .= "Last Name: ".clean_string($last_name)."\n";
+    $email_message .= "Name: ".clean_string($name)."\n";
  
     $email_message .= "Email: ".clean_string($email_from)."\n";
- 
-    $email_message .= "Telephone: ".clean_string($telephone)."\n";
  
     $email_message .= "Comments: ".clean_string($comments)."\n";
  
